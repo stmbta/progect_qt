ebca47ea4700e19c51a60ba7a315df3d1be62c8c
stmbta <73380483+stmbta@users.noreply.github.com>
2020-11-14 19:39:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,49 +14,65 @@ class MyWidget(QMainWindow):
         self.buttonGroup_2.buttonClicked.connect(self.run)
         self.pushButton.clicked.connect(self.reaction)
         self.pushButton_2.clicked.connect(self.cclear)
+        self.pushButton_3.clicked.connect(self.coeff)
+        self.elem1 = None
+        self.elem2 = None
 
-    
+
     def cclear(self):
-        global elem1, elem2, elements
-        self.textbrowser.SetText('')
-        self.textbrowser_2.SetText('')
-        self.textbrowser_3.SetText('')
-        elem1, elem2 = None, None
-        elements = None
+        self.textBrowser.setText('')
+        self.textBrowser_2.setText('')
+        self.textBrowser_3.setText('')
+        self.elem1, self.elem2 = None, None
+        self.elements = None
+
+
+    def coeff(self):
+        con = sqlite3.connect('database.db')
+        cur = con.cursor()
+        result = cur.execute(f"""
+                        SELECT re1, re2, re3 FROM reactions
+                        WHERE elems = ?
+                        """, (self.elements,)).fetchall()
+        self.textBrowser_2.setText(result[0])
+        self.textBrowser.setText(result[2])
+        self.textBrowser_3.setText(result[1])
+        con.commit()
+        con.close()
 
 
     def reaction(self):
-        global elements, elem1, elem2 
+        self.elements = self.elem1 + self.elem2
         con = sqlite3.connect('database.db')
         cur = con.cursor()
-        cur.execute("""
-                        SELECT * FROM table
-                        WHERE elems = elements
-                        """)
-        result = cur.fetchall()
+        result = cur.execute(f"""
+                        SELECT product FROM reactions
+                        WHERE elems = ?
+                        """, (self.elements,)).fetchall()
         if not result:
-            elements = elem2 + elem1
-            cur.execute("""
-                        SELECT * FROM table
-                        WHERE elems = elements)
-                        """)
-            result = cur.fetchall()
+            self.elements = self.elem2 + self.elem1
+            result = cur.execute(f"""
+                        SELECT product FROM reactions
+                        WHERE elems = ?
+                        """, (self.elements,)).fetchall()
         if not result:
-            result = 'компоненты не реагируют'
-        self.texbrowser.SetText(result)
+            result = tuple('компоненты не реагируют')
+        self.textBrowser.setText(result[0])
 
         con.commit()
         con.close()
         
 
-    def run(self):
-        global elem1, elem2, elements
-        if not elem1:
-            elem1 = self.sender().text()
-            self.textbrowser_2.setText(self.sender().text())
+    def run(self, button):
+        if not self.elem1:
+            self.elem1 = button.text()
+            self.textBrowser_2.setText(button.text())
         else:
-            elem2 = self.sender().text()
-            self.textbrowser_3.setText(self.sender().text())
+            self.elem2 = button.text()
+            self.textBrowser_3.setText(button.text())
+    
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
             
         
 
@@ -65,4 +81,5 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
     ex.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec_())
